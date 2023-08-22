5fdbd494ede391bfb21cbd370442c976b8514bb1
Mohamed Hussain H <83116894+hussain033@users.noreply.github.com>
2023-08-22 21:23:36 +0530

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,13 @@
+from predict.py import pre_process, predict
+def predict_api(img_path):
+    image = Image.open(img_path)
+    image = pre_process(image)
+    prediction = predict(image)
+    return prediction
+
+if __name__ == "__main__":
+    if(len(sys.argv) != 2):
+      print("Usage: python3 inference.py /path/to/image")
+    else:
+      path = sys.argv[1]
+      print("The leaf in the image is predicted as"+predict_app(path))
